0af106b41e6ba043aa66d2eb1bab2bc6a608bc55
nbhaohao <nbhaohao@163.com>
2021-05-23 15:40:41 +0800

    feat: 增加函数剩余参数 demo

--- 04.data_types.c
+++ 04.data_types.c
@@ -10,5 +10,9 @@ int main() {
   printf("short int: %lu\n", sizeof(short int));
   printf("int: %lu\n", sizeof(int));
   printf("long int: %lu\n", sizeof(long int));
+  const int kRed = 0xFF0000;
+  const int kRed2 = 0x00FF0000;
+  printf("test %d %lu\n", kRed, sizeof(kRed));
+  printf("test %d %lu\n", kRed2, sizeof(kRed2));;
   return 0;
 }
---
+++ 05.varargs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void HandleVarargs(int arg_count, ...) {
+  va_list args;
+  va_start(args, arg_count);
+  for (int i = 0; i < arg_count; ++i) {
+    int arg = va_arg(args, int);
+    printf("%d: %d\n", i, arg);
+  }
+}
+
+int main(void) {
+  HandleVarargs(3, 1, 2, 3);
+}
